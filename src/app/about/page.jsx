4cdb4735daf20c8a52d0d5e4c9d7c99cb900d91e
pammu453 <111453798+pammu453@users.noreply.github.com>
2024-04-25 11:53:14 +0530

    About page completed

---
+++ src/app/about/page.jsx
@@ -0,0 +1,29 @@
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto px-4 py-8 text-center">
+      <h1 className="text-3xl  mb-4 text-amber-500  font-normal">About IMDb Clone</h1>
+      <p className="text-lg">
+        Welcome to the IMDb Clone! This website is an attempt to replicate
+        the functionality and design of the popular Internet Movie Database
+        (IMDb). Whether you're looking for movie details, actor information,
+        or user reviews, you'll find it here.
+      </p>
+      <p className="text-lg mt-4">
+        This project was created using Next.js, Tailwind CSS, and other
+        modern web technologies. It's intended to be a learning resource for
+        web developers interested in building full-stack applications with
+        React and Next.js.
+      </p>
+      <p className="text-lg mt-4">
+        Please note that the data presented on this website is for
+        demonstration purposes only and is not affiliated with IMDb.
+      </p>
+      <p className="text-lg mt-4">
+        If you have any questions, feedback, or suggestions, feel free to
+        contact us.
+      </p>
+    </div>
+  )
+}
+
+export default About
